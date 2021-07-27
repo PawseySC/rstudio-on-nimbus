dfa8d1a98b2957b7dccf83bc534da5a56835ecf5
Ubuntu <ubuntu@shpc-test.novalocal>
2021-07-27 06:18:28 +0000

    updated run_rstudio.sh

--- run_rstudio.sh
+++ run_rstudio.sh
@@ -12,6 +12,8 @@ Then go to a web browser and enter the following URL to run RStudio: http://loca
 username=ubuntu
 password=what your entered when prompted at the start
 
+All RStudio sessions and associated data will be saved under /data/rstudio.
+
 NOTE: To re-run the rserver command, kill the current process first on your instance, then re-run the Ansible playbook. Kill the process by running: kill $(ps | grep 'rserver' | awk '{print \$1}')
 
 IMPORTANT: After exiting RStudio, close the 8787 port on your local computer****: lsof -ti:8787 | xargs kill -9
